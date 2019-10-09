21e65b9a868f86bdc734e0924691720215bc1155
Marlene Bowles <mbowles@broadway.com>
2019-10-09 12:57:46 -0400

    Makes a yarg command to get the weather

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const axios = require('axios');
+const yargs = require('yargs');
+yargs.version('1.0.2');
 
+/* Sets up Yarg commands */
+const locationObj = {
+    location : {
+        desc: 'Location of where to search weather',
+        type: 'string',
+        demandOption: true,
+    },
+};
 
-async function getLocation () {
+yargs.command('get_weather', 'Finds the location and gets weather', locationObj, (argv) => {
+    getWeather(argv.location);
+}).help().argv;
+
+async function getLocation (loc) {
     try {
-        const response = await axios.get('https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiY29tcHV0YXBhcnMiLCJhIjoiY2sxaTlrNXlpMW5xZTNqbXZwOXZwNnphZiJ9.-DBLUc_ogRzoKw6r6Ecv1w')
+        const response = await axios.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURI(loc)}.json?access_token=pk.eyJ1IjoiY29tcHV0YXBhcnMiLCJhIjoiY2sxaTlrNXlpMW5xZTNqbXZwOXZwNnphZiJ9.-DBLUc_ogRzoKw6r6Ecv1w`)
         if (response.data.features.length == 0){
             throw new Error('Unable to find location, try another search')
         } else {
@@ -18,13 +32,14 @@ async function getLocation () {
     }  
 }
 
-async function getWeather () {
+async function getWeather (loc) {
     try {   
-        const location = await getLocation();
-        axios.get(`https://api.darksky.net/forecast/60cb6a96940d657cabc157cdcf5fe4b7/${location.lat},${location.long}`)
+        const location = await getLocation(loc);
+        const { lat, long, name } = location;
+        axios.get(`https://api.darksky.net/forecast/60cb6a96940d657cabc157cdcf5fe4b7/${lat},${long}`)
             .then((response) => {
                 // success
-                console.log(location.name)
+                console.log(name)
                 console.log(`${response.data.daily.data[0].summary} It is currently ${response.data.currently.temperature} degrees out. There is a ${response.data.currently.precipProbability}% chance of rain.`)
             })
             .catch((err) => {
@@ -40,4 +55,3 @@ async function getWeather () {
     
 }
 
-getWeather()

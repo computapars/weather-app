bdc90592ddd5636731c7f6dcaad50ccc56979297
computapars <computapars@gmail.com>
2019-10-19 18:38:00 -0400

    Cleaned up architecture to add concurrently running scripts

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const express = require('express');
-const weather = require('./../utils/weather');
+const weather = require('./utils/weather');
 const app = express();
 
 app.use(express.static(path.join( __dirname, '../public')))
